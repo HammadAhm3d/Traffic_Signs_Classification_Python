47f43f7fc836880a8b683bb35721ab22b13a8bef
Hammad Ahmed <hammad.a.tahir@gmail.com>
2020-08-08 23:23:16 +0500

    enchanced

--- gui.py
+++ gui.py
@@ -57,9 +57,9 @@ classes = { 1:'Speed limit (20km/h)',
 top=tk.Tk()
 top.geometry('800x600')
 top.title('Traffic sign classification')
-top.configure(background='#CDCDCD')
+top.configure(background='#000000')
 
-label=Label(top,background='#CDCDCD', font=('arial',15,'bold'))
+label=Label(top,background='#000000', foreground='#FFFFFF', font=('arial',15,'bold'))
 sign_image = Label(top)
 
 def classify(file_path):
@@ -72,12 +72,12 @@ def classify(file_path):
     pred = model.predict_classes([image])[0]
     sign = classes[pred+1]
     print(sign)
-    label.configure(foreground='#011638', text=sign) 
+    label.configure(foreground='#FFFFFF', text=sign) 
    
 
 def show_classify_button(file_path):
     classify_b=Button(top,text="Classify Image",command=lambda: classify(file_path),padx=10,pady=5)
-    classify_b.configure(background='#364156', foreground='white',font=('arial',10,'bold'))
+    classify_b.configure(background='#FFFFFF', foreground='black',font=('arial',10,'bold'))
     classify_b.place(relx=0.79,rely=0.46)
 
 def upload_image():
@@ -95,12 +95,12 @@ def upload_image():
         pass
 
 upload=Button(top,text="Upload an image",command=upload_image,padx=10,pady=5)
-upload.configure(background='#364156', foreground='white',font=('arial',10,'bold'))
+upload.configure(background='#FFFFFF', foreground='black',font=('arial',10,'bold'))
 
 upload.pack(side=BOTTOM,pady=50)
 sign_image.pack(side=BOTTOM,expand=True)
 label.pack(side=BOTTOM,expand=True)
 heading = Label(top, text="Know Your Traffic Sign",pady=20, font=('arial',20,'bold'))
-heading.configure(background='#CDCDCD',foreground='#364156')
+heading.configure(background='#000000',foreground='#FFFFFF')
 heading.pack()
 top.mainloop()
